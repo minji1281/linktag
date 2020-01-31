59d33e653841a2f08729d9680bafdf0c7e365dfc
이규찬 <ckeel@naver.com>
2020-01-31 16:51:12 +0900

    test

---
+++ app/src/main/java/com/linktag/linkapp/ui/main/JDMMain.java
@@ -0,0 +1,165 @@
+package com.linktag.linkapp.ui.main;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.linktag.base.base_activity.BaseActivity;
+import com.linktag.base.network.ClsNetworkCheck;
+import com.linktag.base.util.BaseAlert;
+import com.linktag.linkapp.R;
+import com.linktag.linkapp.model.JDMModel;
+import com.linktag.linkapp.network.BaseConst;
+import com.linktag.linkapp.network.Http;
+import com.linktag.linkapp.network.HttpBaseService;
+import com.linktag.linkapp.value_object.JdmVO;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class JDMMain extends BaseActivity {
+
+    private View view;
+    private SwipeRefreshLayout swipeRefresh;
+
+    private JdmRecycleAdapter mAdapter;
+
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+    private ArrayList<JdmVO> mList;
+
+    public JDMMain() {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.fragment_jdm);
+        initLayout();
+        initialize();
+
+    }
+
+//    @Nullable
+//    @Override
+//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        view = inflater.inflate(R.layout.fragment_jdm, container, false);
+//
+//        initLayout();
+//
+//        initialize();
+//
+//
+//        return view;
+//    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        requestJMD_SELECT();
+    }
+
+
+    protected void initLayout() {
+
+        view = findViewById(R.id.recyclerView);
+        recyclerView = view.findViewById(R.id.recyclerView);
+
+
+        swipeRefresh = findViewById(R.id.swipeRefresh);
+//        swipeRefresh.setOnRefreshListener(() -> requestJMD_SELECT());
+        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                requestJMD_SELECT();
+                swipeRefresh.setRefreshing(false);
+            }
+        });
+    }
+
+
+    protected void initialize() {
+        mList = new ArrayList<>();
+        linearLayoutManager = new LinearLayoutManager(mContext);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        mAdapter = new JdmRecycleAdapter(mContext, mList);
+        recyclerView.setAdapter(mAdapter);
+    }
+
+
+    public void requestJMD_SELECT() {
+        // 인터넷 연결 여부 확인
+        if (!ClsNetworkCheck.isConnectable(mContext)) {
+            BaseAlert.show(getString(R.string.common_network_error));
+            return;
+        }
+
+        openLoadingBar();
+
+        //String strToday = ClsDateTime.getNow("yyyyMMdd");
+
+
+        Call<JDMModel> call = Http.jdm(HttpBaseService.TYPE.POST).JDM_SELECT(
+                BaseConst.URL_HOST,
+                "LIST",
+                "1",
+                "",
+                "M191100001"
+        );
+
+
+        call.enqueue(new Callback<JDMModel>() {
+            @SuppressLint("HandlerLeak")
+            @Override
+            public void onResponse(Call<JDMModel> call, Response<JDMModel> response) {
+                Message msg = new Message();
+                msg.obj = response;
+                msg.what = 100;
+
+                new Handler() {
+                    @Override
+                    public void handleMessage(Message msg) {
+                        if (msg.what == 100) {
+                            closeLoadingBar();
+
+                            Response<JDMModel> response = (Response<JDMModel>) msg.obj;
+
+                            mList = response.body().Data;
+                            if (mList == null)
+                                mList = new ArrayList<>();
+
+                            mAdapter.updateData(mList);
+                            mAdapter.notifyDataSetChanged();
+                            swipeRefresh.setRefreshing(false);
+
+                        }
+                    }
+                }.sendMessage(msg);
+            }
+
+            @Override
+            public void onFailure(Call<JDMModel> call, Throwable t) {
+                Log.d("Test", t.getMessage());
+                closeLoadingBar();
+
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/linktag/linkapp/ui/main/Main.java
+++ app/src/main/java/com/linktag/linkapp/ui/main/Main.java
@@ -11,9 +11,11 @@ import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.linktag.base.base_header.BaseHeader;
-import com.linktag.linkapp.ui.work_place_search.FindWorkPlace;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import com.linktag.base.base_activity.BaseActivity;
+import com.linktag.base.base_header.BaseHeader;
 import com.linktag.base.base_view_pager.BaseViewPager;
 import com.linktag.base.base_view_pager.ViewPagerAdapter;
 import com.linktag.base_resource.broadcast_action.ClsBroadCast;
@@ -21,15 +23,10 @@ import com.linktag.linkapp.R;
 import com.linktag.linkapp.ui.login.Login;
 import com.linktag.linkapp.ui.scanner.ScanBarcode;
 import com.linktag.linkapp.ui.settings_main.SettingFragment;
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
-
-
+import com.linktag.linkapp.ui.work_place_search.FindWorkPlace;
 
 import java.io.File;
-
 import java.util.ArrayList;
-
 import java.util.List;
 
 
@@ -38,14 +35,13 @@ public class Main extends BaseActivity {
     private final int TAB_PAGE_COMMENT = 1;
     private final int TAB_PAGE_APPLY = 2;
     private final int TAB_PAGE_SETTING = 3;
-    private final int TAB_PAGE_JDM = 4; //장독테스트
 
     // Variable
     private CommuteFragment fragmentHome;
     private WorkFragment fragmentWork;
     private ApplyFragment fragmentApply;
     private SettingFragment fragmentSetting;
-    private JDMFragment fragmentJdm;
+
 
 
     private BaseViewPager viewPager;
@@ -99,7 +95,14 @@ public class Main extends BaseActivity {
 
         //장독테스트
         tvMainJdm = findViewById(R.id.tvMainJdm);
-        tvMainJdm.setOnClickListener(view -> setCurrentViewPager(TAB_PAGE_JDM));
+
+        tvMainJdm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext, JDMMain.class);
+                startActivity(intent);
+            }
+        });
 
         initViewPager();
     }
@@ -153,7 +156,6 @@ public class Main extends BaseActivity {
         fragmentWork = new WorkFragment();
         fragmentApply = new ApplyFragment();
         fragmentSetting = new SettingFragment();
-        fragmentJdm = new JDMFragment();
 
         fragmentHome.setOnLoadingDialog(callLoadingBar);
         fragmentWork.setOnLoadingDialog(callLoadingBar);
@@ -164,7 +166,6 @@ public class Main extends BaseActivity {
         mListFragment.add(fragmentWork);
         mListFragment.add(fragmentApply);
         mListFragment.add(fragmentSetting);
-        mListFragment.add(fragmentJdm);
 
         mViewPagerAdapter = new ViewPagerAdapter(this.getSupportFragmentManager(), mListFragment);
         viewPager.setAdapter(mViewPagerAdapter);
@@ -226,12 +227,6 @@ public class Main extends BaseActivity {
                 header.btnHeaderText.setVisibility(View.GONE);
                 break;
 
-            case TAB_PAGE_JDM:
-                tvMainJdm.setSelected(true);
-                header.tvHeaderTitle.setText("장독관리");
-                header.btnHeaderRight1.setVisibility(View.GONE);
-                header.btnHeaderText.setVisibility(View.GONE);
-                break;
         }
     }
 
